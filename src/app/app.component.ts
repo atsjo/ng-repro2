06176ca4c8e594d05450e6762dd110fc0065ca76
Atle Sjønøst <atle@teve.no>
2025-05-27 22:53:14 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, CUSTOM_ELEMENTS_SCHEMA, signal  } from '@angular/core';
+import { Component, Injector, CUSTOM_ELEMENTS_SCHEMA, signal, ViewChild, ElementRef, effect } from '@angular/core';
 import { DynamicComponent } from './dynamic.component';
 import { createCustomElement } from '@angular/elements';
 
@@ -9,9 +9,20 @@ import { createCustomElement } from '@angular/elements';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  toggle1 = signal(true);
-  toggle2 = signal(true);
+  @ViewChild('ref', { static: true }) ref!: ElementRef<HTMLElement>;
+  toggle1 = signal(false);
+  toggle2 = signal(false);
+  toggle3 = signal(false);
   constructor(injector: Injector) {
     customElements.define('custom-element-dynamic', createCustomElement(DynamicComponent, { injector }));
+    effect(() => {
+      const active = this.toggle3();
+      if (active) {
+        const el = document.createElement('custom-element-dynamic');
+        this.ref.nativeElement.appendChild(el);
+      } else {
+        this.ref.nativeElement.replaceChildren();
+      }
+    });
   }
 }
